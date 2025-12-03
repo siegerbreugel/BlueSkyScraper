7fcdbbfd60906dd120f74a5e1d1b510e5e1dd4d9
Sieger van Breugel <sieger.van.breugel@fabrique.nl>
2025-12-03 18:47:05 +0100

    Per hour, this is somewhat usable

--- BlueSkyScraper.py
+++ BlueSkyScraper.py
@@ -1,9 +1,22 @@
+"""
+    @Author: Sieger van Breugel, 2025
+
+    Usage:
+        1) Create instance, for example: 'scraper = BlueSkyScraper()'
+        2) Log client into bluesky: 'scraper.login(BLUESKY_USER, BLUESKY_PASSWORD)'
+        3) Use 'scraper.search_posts(query="Mamdani")' to get posts
+"""
+
 import json
+import math
+
+from datetime import timedelta
 from atproto import Client
 
-""" 
-    @Author: Sieger van Breugel, 2025
-"""
+def format_date(date):
+    return date.strftime('%Y-%m-%dT%H:%M:%S.') + f'{int(date.microsecond / 1000):03d}Z'
+
+
 class BlueSkyScraper:
     def __init__(self):
         self.client = Client()
@@ -12,24 +25,56 @@ class BlueSkyScraper:
         self.client.login(user, password)
 
 
+    def query_posts(self, query_dict):
+        try:
+            return self.client.app.bsky.feed.search_posts(query_dict).model_dump_json()
+        except Exception as e:
+            print(e)
+
     """
         Get an amount of posts from the BlueSky search engine,
         Filter using query (a search string), start_date and end_date should be datetime objects.
         The limit is set to 25, but can be customized.
     """
-    def search_posts(self, query, start_date, end_date, limit=25):
-        start = start_date.strftime('%Y-%m-%dT%H:%M:%S.') + f'{int(start_date.microsecond / 1000):03d}Z'
-        end = end_date.strftime('%Y-%m-%dT%H:%M:%S.') + f'{int(end_date.microsecond / 1000):03d}Z'
+    def search_posts(self, query, start_date, end_date, limit=25, per_hour=False):
+        if limit > 100:
+            raise ValueError('Limit cannot be greater than 100')
 
-        try:
-            posts = self.client.app.bsky.feed.search_posts(
-                {'q': query,
-                 'since': str(start),
-                 'until': str(end),
-                 'limit': limit,
-                 }
-            ).model_dump_json()
+        if per_hour:
+            delta = end_date - start_date
+            hours = math.ceil((delta.total_seconds() / 3600))
+            print(f"Searching for {limit} posts per hour for {hours} hour(s)")
 
-            return json.loads(posts)
-        except Exception as e:
-            print(e)
+            posts = []
+            for i in range(hours):
+                start = format_date(start_date + timedelta(hours=i))
+                end = format_date(start_date + timedelta(hours=i+1))
+
+                print(f"Hour {i}, start: {start}, end: {end}")
+
+                query_dict = {
+                    'q': query,
+                    'since': start,
+                    'until': end,
+                    'limit': limit,
+                }
+
+                results = self.query_posts(query_dict)
+
+                posts.extend(json.loads(results)['posts'])
+
+            return posts
+        else:
+            start = format_date(start_date)
+            end = format_date(end_date)
+
+            query_dict = {
+                'q': query,
+                'since': start,
+                'until': end,
+                'limit': limit,
+            }
+
+            posts = self.query_posts(query_dict)
+
+            return json.loads(posts)['posts']
--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
-import datetime
-
-import pandas as pd
+from datetime import datetime
 import pytz
+import json
+
 from dotenv import load_dotenv
 
 from BlueSkyScraper import BlueSkyScraper
@@ -17,11 +17,18 @@ scraper = BlueSkyScraper()
 scraper.login(BLUESKY_USER, BLUESKY_PASSWORD)
 
 # Get the posts
-start_date = datetime.datetime(2025, 10, 16, tzinfo=pytz.timezone('America/New_York'))
-end_date = datetime.datetime(2025, 11, 4, tzinfo=pytz.timezone('America/New_York'))
+# start_date = datetime.datetime(2025, 10, 16, tzinfo=pytz.timezone('America/New_York'))
+# end_date = datetime.datetime(2025, 11, 4, tzinfo=pytz.timezone('America/New_York'))
+start_date = datetime(2025, 12, 1, tzinfo=pytz.timezone('America/New_York'))
+end_date = datetime(2025, 12, 2, tzinfo=pytz.timezone('America/New_York'))
 
-posts = scraper.search_posts('Mamdani', start_date=start_date, end_date=end_date, limit=10)
+posts = scraper.search_posts('Mamdani', start_date=start_date, end_date=end_date, limit=1, per_hour=True)
 
-# Pandas
-df = pd.json_normalize(posts['posts'])
-print(df.head())
+# To convert to dataframe:
+# import pandas as pd
+# df = pd.json_normalize(posts)
+
+# Write to file:
+now = datetime.today().strftime('%Y-%m-%d_%H-%M-%S')
+with open(f"./data/scraped_posts_{now}.json", 'w') as f:
+    json.dump(posts, f)
